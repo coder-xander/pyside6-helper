84de3185b8c1918b30d62e719d799246ea1e2717
十月 <537076443@qq.com>
2023-05-06 16:06:35 +0800

    添加项目界面优化

--- controller/main_window_controller.py
+++ controller/main_window_controller.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import Qt
-from PySide6.QtWidgets import QMainWindow, QWidget, QPushButton, QDialog, QMessageBox
+from PySide6.QtWidgets import QMainWindow, QWidget, QPushButton, QDialog, QMessageBox, QListWidgetItem, QInputDialog
 
 from model.setting_manager import Project, SettingManager
 from view.qt_py_ui_files.ui_main_window import Ui_MainWindow
@@ -13,57 +13,62 @@ class MainWindowController:
         # 现在是不是在添加一个新的项目中
         self.isAddingProjectFlag = False
         self.initAllProject2Ui()
+
     def initAllProject2Ui(self):
+        self.ui.listWidget.doubleClicked.connect(self.onListWidgetItemDoubleClicked)
         settingManager = SettingManager()
         print(settingManager.setting.projects)
         for p in settingManager.setting.projects:
-            widget =  self.project2ProjectWidget(p)
+            widget = self.project2ProjectWidget(p)
             self.ui.listWidget.addItem(p.name)
 
-        #给所有的item绑定槽函数
-
+    def onListWidgetItemDoubleClicked(self, item: QListWidgetItem):
+        for i in range(0, self.ui.stackedWidget.count()):
+            ui: Ui_ProjectSetting = self.ui.stackedWidget.widget(i).ui
+            if ui.pushButton_5.text() == item.text():
+                self.ui.stackedWidget.setCurrentIndex(i)
 
     def showAddProjectDialog(self):
         # 初始化一个空widget进行收集项目的信息
-        dialogUi = Ui_ProjectSetting()
-        dialog = QDialog()
-        dialog.ui = dialogUi
-        dialogUi.setupUi(dialog)
+        widget = self.project2ProjectWidget(Project())
+
         saveBtn = QPushButton()
         saveBtn.setText("save")
         saveBtn.setProperty("func", "save")
         cancelBtn = QPushButton()
         cancelBtn.setText("cancel")
         cancelBtn.setProperty("func", "cancel")
-        dialogUi.widget_3.layout().addWidget(saveBtn)
-        dialogUi.widget_3.layout().addWidget(cancelBtn)
+        widget.ui.widget_3.layout().addWidget(saveBtn)
+        widget.ui.widget_3.layout().addWidget(cancelBtn)
+
         # dialog.setWindowModality(Qt.WindowModality.ApplicationModal)
         def onSaveBtnClicked():
             # 读取界面信息
             project = Project()
-            project.in_ui_files_dir = dialog.ui.lineEdit_5.text()
-            project.out_py_files_dir = dialog.ui.lineEdit_6.text()
-            project.pyside6_uic_path = dialog.ui.lineEdit_8.text()
-            project.pyside6_rcc_path = dialog.ui.lineEdit_9.text()
-            project.pyside6_assistant_path = dialog.ui.lineEdit_12.text()
-            project.pyside6_linguist_path = dialog.ui.lineEdit_16.text()
-            project.pyside6_designer_path = dialog.ui.lineEdit_15.text()
-            project.name = dialog.ui.pushButton_5.text()
+            project.in_ui_files_dir = widget.ui.lineEdit_5.text()
+            project.out_py_files_dir = widget.ui.lineEdit_6.text()
+            project.pyside6_uic_path = widget.ui.lineEdit_8.text()
+            project.pyside6_rcc_path = widget.ui.lineEdit_9.text()
+            project.pyside6_assistant_path = widget.ui.lineEdit_12.text()
+            project.pyside6_linguist_path = widget.ui.lineEdit_16.text()
+            project.pyside6_designer_path = widget.ui.lineEdit_15.text()
+            project.name = widget.ui.pushButton_5.text()
             # if not project.isVaild():
             #     QMessageBox.information(dialog, "info", "Project info is not complete")
             #     return
             self.addOneProject(project)
-            QMessageBox.information(dialog, "info", "add success")
+            QMessageBox.information(widget, "info", "add success")
+            widget.ui.close()
             # 构造Project对象
             pass
 
         def onCancelBtnClicked():
-            dialog.close()
+            widget.ui.close()
             pass
 
         saveBtn.clicked.connect(onSaveBtnClicked)
         cancelBtn.clicked.connect(onCancelBtnClicked)
-        dialog.exec()
+        widget.show()
 
     def run(self):
         if self.ui is None:
@@ -80,11 +85,12 @@ class MainWindowController:
         settingManager = SettingManager()
         settingManager.setting.projects.append(project)
         settingManager.save()
+
     def project2ProjectWidget(self, project: Project):
         WidgetUi = Ui_ProjectSetting()
         widget = QWidget()
         WidgetUi.setupUi(widget)
-        widget.ui = WidgetUi
+        widget.ui: Ui_ProjectSetting = WidgetUi
         widget.ui.lineEdit_5.setText(project.in_ui_files_dir)
         widget.ui.lineEdit_6.setText(project.out_py_files_dir)
         widget.ui.lineEdit_8.setText(project.pyside6_uic_path)
@@ -92,5 +98,10 @@ class MainWindowController:
         widget.ui.lineEdit_12.setText(project.pyside6_assistant_path)
         widget.ui.lineEdit_16.setText(project.pyside6_linguist_path)
         widget.ui.lineEdit_15.setText(project.pyside6_designer_path)
-        widget.ui.lineEdit_5.setText(project.name)
+        widget.ui.pushButton_5.setText(project.name)
+
+        def onProjecNameBtnClicked():
+            projectName = QInputDialog.getText(widget, "input project name", "project name")
+
+        widget.ui.pushButton_5.clicked.connect(onProjecNameBtnClicked)
         return widget
